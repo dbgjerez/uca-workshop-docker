8272915fd90069017acb87940bc5391050f066f4
David Borrego <dborrego@atsistemas.com>
2019-12-02 16:32:31 +0100

    firstcommit

---
+++ src/test/java/com/dbg/uca/jparest/JpaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbg.uca.jparest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
